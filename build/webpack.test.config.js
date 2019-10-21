f10444f05072979511191db8e28a281f417020cf
Dani <daniel@imaginaendigital.es>
2019-10-21 14:30:43 +0200

    first commit

---
+++ build/webpack.test.config.js
@@ -0,0 +1,72 @@
+const path = require("path");
+const vueLoaderConfig = require("./vue-loader.conf");
+const nodeExternals = require("webpack-node-externals");
+
+let rules = [
+	{
+		test: /\.(js|vue)$/,
+		loader: "eslint-loader",
+		enforce: "pre",
+		include: [path.resolve("src")],
+		options: {
+			formatter: require("eslint-friendly-formatter")
+		}
+	},
+	{
+		test: /\.vue$/,
+		loader: "vue-loader",
+		include: [path.resolve("src"), path.resolve("test")],
+		exclude: /node_modules/,
+		options: vueLoaderConfig
+	},
+	{
+		test: /\.js$/,
+		loader: "babel-loader",
+		include: [path.resolve("src"), path.resolve("test")],
+		exclude: /node_modules/
+	},
+	{
+		test: /\.(woff2?|svg)$/,
+		loader: "url-loader",
+		include: [path.resolve("src"), path.resolve("test")]
+	},
+	{
+		test: /\.(ttf|eot)$/,
+		loader: "url-loader",
+		include: [path.resolve("src"), path.resolve("test")]
+     },
+     {
+          test: /\.css$/,
+          loader:[ 'style-loader', 'css-loader' ],
+     }
+];
+
+module.exports = {
+	devtool: "inline-cheap-module-source-map",
+
+	entry: "./src/index.js",
+
+	output: {
+		path: path.resolve("dist"),
+		filename: "vfg.js",
+		library: "VueFormGenerator",
+		libraryTarget: "umd"
+	},
+
+	module: {
+		rules
+	},
+
+	plugins: [],
+
+	resolve: {
+		aliasFields: ["browser"],
+		extensions: [".js", ".vue", ".json", ".css"],
+		alias: {
+			vue$: "vue/dist/vue.esm.js",
+			src: path.resolve("src")
+		}
+	},
+
+	externals: [nodeExternals()]
+};
---
+++ src/fields/optional/fieldDateTimePicker.vue
@@ -0,0 +1,95 @@
+<template>
+
+          <datetime  
+          type="datetime"
+          v-model="value"          
+          use12-hour
+          moment-locale="es"
+          :format="schema.format"
+          :disabled="disabled"
+          :placeholder="schema.placeholder"
+          :readonly="schema.readonly"
+          :min-datetime="schema.min"
+          :max-datetime="schema.max"
+          ></datetime>
+
+</template>
+
+<script>
+import abstractField from "../abstractField";
+import dateFieldHelper from "../../utils/dateFieldHelper";
+
+import {Datetime} from "vue-datetime";
+
+// You need a specific loader for CSS files
+import "vue-datetime/dist/vue-datetime.css";
+
+
+let inputFormat = "YYYY-MM-DD HH:mm:ss";
+
+export default {
+     mixins: [abstractField],
+     components: {
+          Datetime
+     },
+
+	methods: {
+		getDateFormat() {
+			if (this.schema && this.schema.format) return this.schema.format;
+			else return inputFormat;
+		},
+		...dateFieldHelper
+	},
+
+	mounted() {
+
+	},
+
+	beforeDestroy() {
+
+	}
+};
+</script>
+
+
+<style>
+.vdatetime-input {
+     display: block;
+     width: 100%;
+     height: calc(1.5em + 0.75rem + 2px);
+     padding: 0.375rem 0.75rem;
+     font-size: 1rem;
+     font-weight: 400;
+     line-height: 1.5;
+     color: #495057;
+     background-color: #fff;
+     background-clip: padding-box;
+     border: 1px solid #ced4da;
+     border-radius: 0.25rem;
+     -webkit-transition: border-color 0.15s ease-in-out,
+          -webkit-box-shadow 0.15s ease-in-out;
+     transition: border-color 0.15s ease-in-out,
+          -webkit-box-shadow 0.15s ease-in-out;
+     transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+     transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out,
+          -webkit-box-shadow 0.15s ease-in-out;
+}
+.vdatetime-popup__header {
+     background: #031561;
+}
+.vdatetime-month-picker__item--selected {
+     color: #031561;
+}
+.vdatetime-popup__actions__button {
+     color: #031561;
+}
+.vdatetime-year-picker__item--selected {
+     color: #031561;
+     font-size: 32px;
+}
+.vdatetime-calendar__month__day--selected > span > span,
+.vdatetime-calendar__month__day--selected:hover > span > span {
+     background: #031561;
+}
+
+</style>
